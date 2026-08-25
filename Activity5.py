ee3d2c9d7d266fcd3bc9542d77a1880ee11970c5
kingramos798 <rafaelramirezzz368@gmail.com>
2026-08-25 18:45:41 +1000

    Add files via upload

---
+++ Activity5.py
@@ -0,0 +1,23 @@
+a = 74
+
+b = 12
+
+sum = a + b
+
+diff = a - b
+
+
+
+print("The sum of ", a, "plus",b,"is", sum)
+
+print("The sum of ",a, "plus",b,"is", diff)
+
+print("The sum of ",a, "times",b,"is", a*b)
+
+print("The sum of ", a, "division", b, "is", a/b)
+
+print("The sum of ",a, "round down", b, "is", a//b)
+
+print("The sum of ",a, "exponents", b, "is", a**b)
+
+print("The sum of ",a,"modulus", b, "is", a%b)
