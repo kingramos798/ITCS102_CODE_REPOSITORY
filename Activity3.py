74f85563ebd5b1fd2f3c957e843e7a465c2253f9
kingramos798 <rafaelramirezzz368@gmail.com>
2026-08-27 22:26:32 +1000

    Add files via upload

---
+++ Activity3.py
@@ -0,0 +1,19 @@
+#integer variable
+age = "18"
+
+#float variable
+height = "180.56"
+weight = "65.48"
+
+#string variable
+name = "King Rafael Ramos"
+
+#Boolean Variable
+isSingle = True
+
+#print
+
+print("hi, my name is", name)
+print("my age is", age)
+print("are you single?", isSingle)
+print("my height and weight is", height, "and", weight)
