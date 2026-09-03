2aa4654f67b240cd3622dd4135949d9c536e03a5
kingramos798 <rafaelramirezzz368@gmail.com>
2026-09-03 16:48:38 +1000

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,50 @@
+total = 0
+
+n1 = eval (input("Enter a Number -->"))
+total = total + n1
+
+n2 = eval (input("Enter a Number -->"))
+total + total + n2
+
+#repeat 10x
+
+n2 = eval(input("Enter a Number -->"))
+total += n2
+
+n2 = eval(input("Enter a Number -->"))
+total -= n2
+
+n2 = eval(input("Enter a Number -->")) 
+total *= n2
+
+n2 = eval(input("Enter a Number -->"))
+total /= n2
+
+n2= eval(input("Enter a Number -->"))
+total *= n2
+
+n2= eval(input("Enter a Number -->"))
+total += n2
+
+n2 = eval(input("Enter a Number -->"))
+total -= n2
+
+n2 = eval(input("Enter a Number -->"))
+total *= n2
+
+n2 = eval(input("Enter a Number -->"))
+total /= n2
+
+n2 = eval(input("Enter a Number -->"))
+total *= n2
+
+n2 = eval(input("Enter a Number -->"))
+total += n2
+
+n2 = eval(input("Enter a Number -->"))
+total *= n2
+
+n2 = eval(input("Enter a Number -->"))
+total -= n2
+
+print("The sum is", total)
