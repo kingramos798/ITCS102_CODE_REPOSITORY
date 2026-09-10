0a43fbeff8854cb0d98efe20e0b74f24a98b17d1
kingramos798 <rafaelramirezzz368@gmail.com>
2026-09-10 12:54:44 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,20 @@
+Name = input("Write your Name: ")
+
+Item = input("Your name of Item:")
+
+is_Fragile = bool(input("The Item is Fragile: "))
+
+Weight = float(input("The Weight of Item:"))
+
+Distance = float(input("The Distance of Item: "))
+
+is_express = bool(input("The Item is Badly Needed:"))
+
+is_international = bool(input("The Item is Ship Internationaly:"))
+
+
+
+if Weight >= 1:  
+    print('It will be free')
+
+
