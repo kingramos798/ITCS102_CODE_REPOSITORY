7788d3c9948f198f28299833db25bc460c126475
kingramos798 <rafaelramirezzz368@gmail.com>
2026-09-10 12:20:57 +0800

    Add files via upload

---
+++ Activity9.py
@@ -0,0 +1,16 @@
+#relational operator demo
+x = 8
+y = 4
+
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+print(x == y)
+
+text1 = "King"
+text2 = "king"
+
+print(text1 == text2)
+
+
---
+++ activity10.py
@@ -0,0 +1,10 @@
+a = 15
+b = 4
+c = 12
+
+print(a > b or c < a)
+print(a > b or c < a and c==b and a > b or c and a < b)
+print(c == a)
+print(b < a or b==a)
+print(b == a and c > c)
+
